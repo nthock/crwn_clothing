57dc31e7e5627f43ac2667d7580934dbd5a00fe2
Thiam Hock Ng <nthock@gmail.com>
2019-12-03 23:34:47 +0800

    Update MenuItem to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 
+const Hats = () => {
+  return <div>HATS Page</div>;
+};
+
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/hats" component={Hats} />
       </Switch>
     </div>
   );
