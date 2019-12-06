b51506cdda478e4dc74c6dd76cb21be6f8ae97cd
Thiam Hock Ng <nthock@gmail.com>
2019-12-07 02:43:17 +0800

    Implement firebase utils included ability to store authenticated users in firebase

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -6,8 +6,8 @@ const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
   <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <div className="background-image" style={{ backgroundImage: `url(${imageUrl})`}} />
     <div className="content">
-      <h1 class="title">{title.toUpperCase()}</h1>
-      <span class="subtitle">SHOP NOW</span>
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
 );
