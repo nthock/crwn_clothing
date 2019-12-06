5a2637e813c5ff237345ed64df9051a2d7773d6f
Thiam Hock Ng <nthock@gmail.com>
2019-12-07 02:48:44 +0800

    Finish sign-in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -28,8 +28,6 @@ class App extends React.Component {
           this.setState({
             currentUser: { id: snapShot.id, ...snapShot.data() }
           });
-
-          console.log(this.state);
         });
       }
       this.setState({ currentUser: userAuth });
